f05d4c28444c81a5711449e4d250b5d69c777c9a
Patrick Reid <preid22gmail.com>
2024-02-09 09:01:06 -0500

    Added cell elements to board div with cell ID#s

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -4,7 +4,8 @@
  a seperate init.
  */
 
-
+let mainBoard = document.getElementsByClassName("board");
+let playerInfo = document.getElementsByClassName("players");
 
 const gameBoard = (() => {
   let grid = [
@@ -22,7 +23,6 @@ const gameBoard = (() => {
   /*Tried writing newGrid just as a variable re-assignment, this didn't work, needed to actually
   iterate over the array and re-assign indiv. elems., thinking this is a deep/shallow copy issue*/
 
-
   const drop = (token, x, y) => {
     if (grid[x][y] === "X" || grid[x][y] === "Y") {
       console.log("Pick another space");
@@ -34,4 +34,32 @@ const gameBoard = (() => {
   return { grid, newGrid, drop };
 })();
 
+let gameMechanics = () => {
+  const isCellEmpty = (gridX, gridY) => {
+    if (gameBoard.grid[gridX][gridY] === "") return true;
+  };
+  const checkForWin = () => {
+    let board = gameBoard.grid;
 
+    for (let i = 0; i < 3; i++) {
+      if (board[i][0] !== "" && board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+        return true;
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (board[0][i] !== "" && board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+        return true;
+      }
+    }
+
+    if (board[0][0] !== "" && board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+      return true;
+    }
+    if (board[0][2] !== "" && board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+      return true;
+    }
+
+    return false;
+  };
+};
