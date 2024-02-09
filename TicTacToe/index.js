eb62da180dcd337fc4662748eb7d728045af5864
Patrick Reid <preid22gmail.com>
2024-02-09 10:44:01 -0500

    Refactored comments, added checkCell func

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,7 +1,7 @@
 let mainBoard = document.getElementsByClassName("board");
 let playerInfo = document.getElementsByClassName("players");
 
-const boardChange = (() => {
+const boardControl = (() => {
   /*comment1*/
   let gridStateArray = [
     ["", "", ""],
@@ -9,7 +9,11 @@ const boardChange = (() => {
     ["", "", ""],
   ]; /*comment2*/
 
-  const newGrid = () => {
+  const checkCell = (x, y) => {
+    return gridStateArray[x][y] === "X" || gridStateArray[x][y] === "Y";
+  };
+
+  const resetGrid = () => {
     for (let i = 0; i < gridStateArray.length; i++) {
       for (let j = 0; j < gridStateArray[0].length; j++) {
         gridStateArray[i][j] = "";
@@ -26,55 +30,8 @@ const boardChange = (() => {
     }
   };
 
-  return { gridStateArray, newGrid, drop };
+  return { gridStateArray, resetGrid, drop };
 })();
-
-let gameMechanics = () => {
-  const isCellEmpty = (gridX, gridY) => {
-    if (gameBoard.gridStateArray[gridX][gridY] === "") return true;
-  };
-  const checkForWin = () => {
-    let board = gameBoard.gridStateArray;
-
-    for (let i = 0; i < 3; i++) {
-      if (
-        board[i][0] !== "" &&
-        board[i][0] === board[i][1] &&
-        board[i][0] === board[i][2]
-      ) {
-        return true;
-      }
-    }
-
-    for (let i = 0; i < 3; i++) {
-      if (
-        board[0][i] !== "" &&
-        board[0][i] === board[1][i] &&
-        board[0][i] === board[2][i]
-      ) {
-        return true;
-      }
-    }
-
-    if (
-      board[0][0] !== "" &&
-      board[0][0] === board[1][1] &&
-      board[0][0] === board[2][2]
-    ) {
-      return true;
-    }
-    if (
-      board[0][2] !== "" &&
-      board[0][2] === board[1][1] &&
-      board[0][2] === board[2][0]
-    ) {
-      return true;
-    }
-
-    // No win
-    return false;
-  };
-};
 
 /*
 COMMENTS:
@@ -88,6 +45,6 @@ COMMENTS:
   ENCAPSULATING the gridStateArray within the object means that it is only accessible
   by the methods we allow
 
-  - COMMENT3: Tried writing newGrid just as a variable re-assignment, this didn't work, needed to actually
+  - COMMENT3: Tried writing resetGrid just as a variable re-assignment, this didn't work, needed to actually
   iterate over the array and re-assign indiv. elems., thinking this is a deep/shallow copy issue
 */
